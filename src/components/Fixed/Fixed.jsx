58fba519cf99977b42cd6a8a4c39a1e425b39246
Culee <thanhdongtran77@gmail.com>
2023-09-22 10:17:59 +0700

    con reponsive

---
+++ src/components/Fixed/Fixed.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Fixed.css';
+
+import { CiSettings } from 'react-icons/ci';
+import { AiOutlineHeart } from 'react-icons/ai';
+import { BsBook } from 'react-icons/bs';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { BiSolidChevronUp } from 'react-icons/bi';
+
+const Fixed = () => {
+   return (
+      <div className="fixed">
+         <div className="fixed-icons">
+            <div>
+               <CiSettings className="fixed-icons-ic" />
+            </div>
+            <div>
+               <AiOutlineHeart className="fixed-icons-ic" />
+            </div>
+            <div>
+               <BsBook className="fixed-icons-ic" />
+            </div>
+            <div>
+               <AiOutlineShoppingCart className="fixed-icons-ic" />
+            </div>
+         </div>
+         <div className="fixed-scoll">
+            <BiSolidChevronUp />
+         </div>
+      </div>
+   );
+};
+
+export default Fixed;
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,90 @@
 import React from 'react';
 import './Footer.css';
+
+import awrds from '../../assests/img/awards.png';
+import phat from '../../assests/img/phat.jpg';
+import chimcut from '../../assests/img/chimcut.jpg';
+
+import { MdOutlinePhoneIphone } from 'react-icons/md';
+import { LiaMapMarkerAltSolid } from 'react-icons/lia';
+import { GiAlarmClock } from 'react-icons/gi';
+
 const Footer = () => {
    return (
       <div className="footer">
          <div className="footer-top">
             <div className="footer-info">
-               <div className="footer-nav">1</div>
-               <div className="footer-nav">2</div>
-               <div className="footer-nav">3</div>
+               <div className="footer-nav">
+                  <h3>Our Awards</h3>
+                  <p>
+                     London is a megalopolis of people, ideas and frenetic energy. The capital and largest city of the
+                     United Kingdom.
+                  </p>
+                  <img src={awrds} alt="" />
+               </div>
+               <div className="footer-nav">
+                  <div className="container-contact">
+                     <h3>Contact Info</h3>
+                     <div className="contacts">
+                        <div className="contact">
+                           <MdOutlinePhoneIphone />
+                           <p className="textwidget">0869163113</p>
+                        </div>
+                        <div className="contact">
+                           <LiaMapMarkerAltSolid />
+                           <p className="textwidget">Me Tri Nam Tu Liem Ha noi</p>
+                        </div>
+                        <div className="contact">
+                           <GiAlarmClock />
+                           <p className="textwidget">Mon - Sat 9.00 - 18.00</p>
+                        </div>
+                     </div>
+                     <div className="icons-contact">
+                        <i class="fa-brands fa-square-facebook"></i>
+                        <i class="fa-brands fa-twitter"></i>
+                        <i class="fa-brands fa-youtube"></i>
+                        <i class="fa-brands fa-pinterest"></i>
+                        <i class="fa-brands fa-instagram"></i>
+                     </div>
+                  </div>
+               </div>
+               <div className="footer-nav">
+                  <h3>Recent Trips</h3>
+                  <div className="recent">
+                     <div className="recent-item">
+                        <img src={phat} alt="" />
+                     </div>
+                     <div className="recent-item">
+                        <img src={chimcut} alt="" />
+                     </div>
+                     <div className="recent-item">
+                        <img src={phat} alt="" />
+                     </div>
+                     <div className="recent-item">
+                        <img src={chimcut} alt="" />
+                     </div>
+                     <div className="recent-item">
+                        <img src={phat} alt="" />
+                     </div>
+                     <div className="recent-item">
+                        <img src={chimcut} alt="" />
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div className="footer-bootom">
+            <div className="container-bootom">
+               <div className="copyright">
+                  <span> ©Copyright Grand Tour Theme Demo - Theme by ThemeGoods</span>
+               </div>
+               <div className="copyleft">
+                  <span>Home</span>
+                  <span>Tour</span>
+                  <span>Blog</span>
+                  <span>Purchse</span>
+                  <span>Theme</span>
+               </div>
             </div>
          </div>
       </div>
